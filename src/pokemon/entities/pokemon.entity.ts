0bc3b42430807936caa7acca2ba4569a83aeb336
juanoxi <jm.oxinalde@hotmail.com>
2024-08-07 22:11:35 -0300

    Se crea el schemaPokemon en mongo, Se configura en pokemon.module imports del schema para que lo genere en la bd. en la entidad pokemon extiende de Documents y se crea el SchemaPokemon, luego se aplican restricciones a los atributois de pokemon como name unique e indexacion para buscarlo mas rapido.

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,18 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+// al extender de document de mongoose se indica que esta entidad es un document de mongo. El schema va a definir que columans usa la entidad. @Schema Se mapea con la BD. 
+@Schema()
+export class Pokemon extends Document {
+
+    //mongoid : string lo genera mongo de forma automatica. es unico globalmente.
+    // prop define reglas de negocio, por ej que sean unicos los atributos, not null. El indice sabe donde esta este documento.
+    @Prop({unique:true, index:true})
+    name:string;
+
+    @Prop({unique:true, index:true})
+    nro:number;
+
+}
+
+// Creamos el schema.
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
